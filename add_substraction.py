295f21302d953a409e12edace7e88b1420427b4f
CharlesCrittenden <167738478+CharlesCrittenden@users.noreply.github.com>
2024-04-22 00:29:56 -0500

    Add files via upload

---
+++ add_substraction.py
@@ -0,0 +1,13 @@
+#Charles Crittenden
+#adding and subtraction of two numbers
+
+no1 = input('What is your first number?\n')
+no2 = input('What is your second nunmber?\n')
+addnum = int(no1)+int(no2)
+subtractnum = int(no1)-int(no2)
+
+print ('Adding the two numbers: %s.' % addnum)
+print ('Subtracting the two numbers: %s.' % subtractnum)
+
+
+name = input('done\n' )
