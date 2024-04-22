08b4cfb798fe0bcfdf7131970d14c1c9ff2f2a96
CharlesCrittenden <167738478+CharlesCrittenden@users.noreply.github.com>
2024-04-22 00:30:20 -0500

    Add files via upload

---
+++ multi_div.py
@@ -0,0 +1,13 @@
+#Charles Crittenden
+#mutilpy and division
+
+no1 = input('What is your first number?\n')
+no2 = input('What is your second nunmber?\n')
+
+multinum = int(no1)*int(no2)
+divnum = int(no1)/int(no2)
+
+print ('Multiply the two numbers: %s.' % multinum)
+print ('Dividing the two numbers: %s.' % divnum)
+
+name = input('done\n' )
